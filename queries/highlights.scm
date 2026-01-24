1f5443e943fa7b88eb3a8da4cb84ff1b7aef68a2
wearrrrr <contact@wearr.dev>
2026-01-24 16:45:42 -0600

    Simplify highlights query to only use valid node types

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,31 +1,3 @@
-; Keywords
-[
-  "func"
-  "let"
-  "global"
-  "if"
-  "else"
-  "while"
-  "for"
-  "in"
-  "switch"
-  "case"
-  "default"
-  "try"
-  "catch"
-  "finally"
-  "menu"
-  "import"
-] @keyword
-
-; Control flow keywords
-[
-  "return"
-  "break"
-  "continue"
-  "throw"
-] @keyword.control
-
 ; Functions
 (function_declaration
   name: (identifier) @function)
@@ -51,65 +23,17 @@
 (pair
   key: (identifier) @property)
 
-; Operators
-[
-  "="
-  "+"
-  "-"
-  "*"
-  "/"
-  "%"
-  "**"
-  "=="
-  "!="
-  "<"
-  "<="
-  ">"
-  ">="
-  "&&"
-  "||"
-  "!"
-  "@"
-  "??"
-  "++"
-  "--"
-] @operator
-
-; Punctuation
-[
-  "("
-  ")"
-  "{"
-  "}"
-  "["
-  "]"
-] @punctuation.bracket
-
-[
-  ","
-  ":"
-  ";"
-] @punctuation.delimiter
-
-[
-  "?"
-] @punctuation.special
-
 ; Literals
 (number) @number
 (string) @string
 (boolean) @boolean
 (null) @constant.builtin
 
-; String interpolation
-(string_interpolation) @string
-(string_content) @string
-(interpolation
-  "{" @punctuation.special
-  "}" @punctuation.special)
-
 ; Comments
 (comment) @comment
+
+; Identifiers (fallback)
+(identifier) @variable
 
 ; Identifiers (fallback)
 (identifier) @variable
